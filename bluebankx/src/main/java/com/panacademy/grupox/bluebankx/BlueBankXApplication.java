ef7677b790d7aaf8ad3b952e362fc7b3d3e1f9c7
Yuri Jivago <yurijivagoads@gmail.com>
2021-11-09 20:20:21 -0300

    Estrutura base do projeto final - Grupo X

---
+++ bluebankx/src/main/java/com/panacademy/grupox/bluebankx/BlueBankXApplication.java
@@ -0,0 +1,13 @@
+package com.panacademy.grupox.bluebankx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBankXApplication.class, args);
+	}
+
+}
---
+++ bluebankx/src/test/java/com/panacademy/grupox/bluebankx/BlueBankXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panacademy.grupox.bluebankx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBankXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

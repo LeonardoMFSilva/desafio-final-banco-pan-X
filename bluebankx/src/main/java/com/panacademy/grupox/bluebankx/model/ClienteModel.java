17d8f9b2974f2a57e0d119dea799e0c69f68e8a4
Maria Julia <mariajuliamuniz@ufpi.edu.br>
2021-11-12 17:17:51 -0300

    atualizando a classe ClienteModel

--- bluebankx/src/main/java/com/panacademy/grupox/bluebankx/model/ClienteModel.java
+++ bluebankx/src/main/java/com/panacademy/grupox/bluebankx/model/ClienteModel.java
@@ -1,5 +1,159 @@
 package com.panacademy.grupox.bluebankx.model;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class ClienteModel {
 
+    private String cpf;
+    private String nome;
+    private LocalDate dataNasc;
+    private String email;
+    private String telefone;
+    private String profissao;
+    private BigDecimal renda;
+    private BigDecimal patrimonio;
+
+    //Endereço
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private Integer numero;
+    private String complemento;
+    private String cidade;
+    @Enumerated(EnumType.STRING)
+    private UF uf;
+
+    // CONTA
+    private String numConta;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public BigDecimal getRenda() {
+        return renda;
+    }
+
+    public void setRenda(BigDecimal renda) {
+        this.renda = renda;
+    }
+
+    public BigDecimal getPatrimonio() {
+        return patrimonio;
+    }
+
+    public void setPatrimonio(BigDecimal patrimonio) {
+        this.patrimonio = patrimonio;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public UF getUf() {
+        return uf;
+    }
+
+    public void setUf(UF uf) {
+        this.uf = uf;
+    }
+
+    public String getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(String numConta) {
+        this.numConta = numConta;
+    }
 }
